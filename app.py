4b2dcec4e128606d34261fed8b06f5cd883f598f
Amisha <57953781+amisha200@users.noreply.github.com>
2021-10-01 02:27:34 +0530

    Fixed model pipeline with sidebar menu

--- app.py
+++ app.py
@@ -33,14 +33,16 @@ data = [("my unit test failed","software"),
 ("i swapped the memory","hardware"),
 ("i tested the code","software")]
 
-<!------------>
+# Model building
+model = Pipeline(('vectorizer', BagOfWords(lowercase=True)),('nv',MultinomialNB()))
+for x,y in data:
+    model = model.learn_one(x,y)
 
 
-
-
-
-
-
+#Storage in a database
+import sqlite3
+conn = sqlite3.connect('data.db')
+c = conn.cursor()
 
 
 # Create Fxn from SQL 
@@ -69,7 +71,93 @@ def main():
     menu = ["Home", "Manage", "About"]
     create_table()
 
-    <!---------->
+   choice = st.sidebar.selectbox("Menu",menu)
+    if choice == "Home":
+        st.subheader("Home")
+        with st.form(key='mlform'):
+            col1,col2 = st.beta_columns([2,1])
+            with col1:
+                message = st.text_area("Message")
+                submit_message = st.form_submit_button(label='Predict')
+
+            with col2:
+                st.write("Online Incremental ML")
+                st.write("Predict Text as software or hardware related")
+
+        if submit_message:
+            prediction = model.predict_one(message)
+            prediction_proba = model.predict_proba_one(message)
+            probability = max(prediction_proba.values())
+            postdate = datetime.now()
+
+            # Add Data To Database
+            add_data(message,prediction,probability,prediction_proba['software'],prediction_proba['hardware'],postdate)
+            st.success("Data Submitted")
+
+
+
+            res_col1 ,res_col2 = st.beta_columns(2)
+            with res_col1:
+                st.info("Original Text")
+                st.write(message)
+
+                st.success("Prediction")
+                st.write(prediction)
+
+            with res_col2:
+                st.info("Probability")
+                st.write(prediction_proba)
+
+                #Plot of probability
+                df_proba = pd.DataFrame({'label':prediction_proba.keys(),'probability':prediction_proba.values()})
+                st.dataframe(df_proba)
+                #visualization
+                fig = alt.Chart(df_proba).mark_bar().encode(x='label',y='probability')
+                st.altair_chart(fig,use_container_width=True)
+
+
+
+
+    elif choice == ("Manage"):
+        st.subheader("Manage & Monitor Results")
+        stored_data = view_all_data()
+        new_df = pd.DataFrame(stored_data,columns=['message','prediction','probability','software_proba','hardware_proba','postdate'])
+        st.dataframe(new_df)
+        new_df['postdate'] = pd.to_datetime(new_df['postdate'])
+
+        c = alt.Chart(new_df).mark_line().encode(x='postdate', y='probability')
+        st.altair_chart(c)
+
+
+        c_software_proba = alt.Chart(new_df['software_proba'].reset_index()).mark_line().encode(x='software_proba',y='index')
+        c_hardware_proba = alt.Chart(new_df['hardware_proba'].reset_index()).mark_line().encode(x='hardware_proba',y='index')
+
+        c1,c2 = st.beta_columns(2)
+        with c1:
+            with st.beta_expander("Software Probability"):
+                st.altair_chart(c_software_proba,use_container_width=True)
+
+        with c2:
+            with st.beta_expander("Hardware Probability"):
+                st.altair_chart(c_hardware_proba,use_container_width=True)
+
+        with st.beta_expander("Prediction distribution"):
+            fig2 = plt.figure()
+            sns.countplot(x='probability', data=new_df)
+            st.pyplot(fig2)
+
+
+
+    else:
+        st.subheader("About")
+        st.write("""
+        This is a *ML App* to classify & predict software or hardware text.
+        """)
+
+
+
+
+
       
 
 if __name__ == '__main__':
